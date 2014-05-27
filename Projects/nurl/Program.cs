f86a67ce6800fd946442e02ac1f8d5c6446c0f0f
piratebreizh <alexandre.d.bzh@gmail.com>
2014-05-27 11:05:59 +0200

    First comit, création du projet sous SharpDevelop

---
+++ Projects/nurl/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Elisabeth LY
+ * Date: 27/05/2014
+ * Time: 10:56
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace nurl
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
