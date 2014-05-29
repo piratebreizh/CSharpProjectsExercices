6bfd7045ff1e0e4f6fc9c666fa6a24726383f50a
piratebreizh <alexandre.d.bzh@gmail.com>
2014-05-29 15:25:27 +0200

    Version avec test oublie dans le précédent PUSH

---
+++ Projects/nurl/Nurl.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace nurl
+{
+	/// <summary>
+	/// Description of Nurl.
+	/// </summary>
+	public class Nurl
+	{
+		
+		const int typeGet = 1;
+		const int typeTest = 2;
+		
+		
+		public Nurl()	
+		{
+		}
+		
+		public string getContent(string URL)
+		{
+			return "<h1>hello</h1>";
+		}
+		
+		public void saveContent(string URL)
+		{
+			
+		}
+		
+		public void showContent(string URL)
+		{
+			
+		}
+		
+		
+		public void Get(string[] args)
+		{
+			if(args.Length==5){
+				if(args[3].Equals("-save")){
+					
+				}
+			}else if (args.Length == 3){
+				
+			}
+			var loadindSourceHtmlPage(args[3]));
+		}
+		
+		public string loadindSourceHtmlPage(string url)
+		{
+			
+			string htmlCode = null;
+			using (WebClient client = new WebClient ())
+			{
+				if(client != null){
+			    	htmlCode = client.DownloadString(url);
+				}
+			}
+			return htmlCode;
+		}
+		
+		public Boolean isURL(string URL)
+		{
+			Match match = Regex.Match(URL, @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)",RegexOptions.IgnoreCase); 
+			if(match.Success){
+				return true;
+			}else{
+				return false;	
+			}
+			
+		}
+		
+		public Boolean isArgsCorrect(string[] args)
+		{
+			if(args != null){
+				if(args.Length>=3){
+					if(args[0].Equals("test") || args[0].Equals("get")){
+						if(args[1].Equals("-url")){
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
+		
+		public void routage(string[] args)
+		{
+			if(this.isURL(args[2])){
+				if(args[0].Equals("get")){
+					this.Get(args);
+				}else if(args[0].Equals("test")){
+					//this.Test(args);
+				}
+				
+				
+			}
+		}
+	}
+}
---
+++ Projects/nurl/NurlTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace nurl
+{
+	[TestFixture]
+	public class NurlTest
+	{
+		
+		
+		static String [] argsOkPourGet = {"get","-url","https://www.youtube.com/watch?v=o1tj2zJ2Wvg"};
+		static String [] argsOkPourTest = {"test","-url","https://www.youtube.com/watch?v=o1tj2zJ2Wvg"};
+		
+		
+		[Test]
+		public void isArgsCorrectTest()
+		{
+			//given
+			var nurl = new Nurl();
+			
+			//when
+			var result = nurl.isArgsCorrect(argsOkPourGet); 
+			
+			//then
+			Assert.IsTrue(result,"C'est bien un get ou un test");
+		}
+		
+		/**Test pour la fonction Nurl.isURL
+		* 
+		*/
+		[Test]
+		public void testValiderAdresseURL()
+		{
+			//given
+			string url = argsOkPourGet[2];
+			var nurl = new Nurl();
+			
+			//when
+			var isURL = nurl.isURL(url);						
+			
+			//then
+			Assert.IsTrue(isURL);
+		}
+		
+		
+		/** Test afin de savoir si le code source de la page HTML à bien été retourné
+		 * 
+		*/
+		[Test]
+		public void testRetournSource(){
+			//given
+			string url = argsOkPourGet[2];
+			var nurl = new Nurl();
+			
+			//when
+			var contentHTML = nurl.loadindSourceHtmlPage(url);
+			
+			//then
+			Assert.NotNull(contentHTML);
+		}
+		
+	}
+}
